cf88b5b79abe94d081c012c5598859063c37d259
RajAutomationLab <96832237+RajAutomationLab@users.noreply.github.com>
2024-01-29 23:26:01 +0530

    Create Test.java

---
+++ Test.java
@@ -0,0 +1,11 @@
+public class Test{
+  public static void main(String[] args){
+    System.out.println("Hello GitHub...");
+    sayHelloToAll("Good Morning...");
+  }
+
+  void sayHelloToAll(String message){
+     System.out.println(message+" Everyone...This is Raj from Bharat");
+  }
+}
+  
